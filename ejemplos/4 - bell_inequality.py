16512c9ac008843f0a7ecd3f0d818f23eb3f88c7
agustinsilva447 <agustinsilva447@gmail.com>
2020-06-11 20:49:41 -0300

    bell inequality

--- ejemplos/4 - bell_inequality.py
+++ ejemplos/4 - bell_inequality.py
@@ -20,9 +20,41 @@ Win rate: 84.0%
 """
 
 import numpy as np
-
 import cirq
 
+def make_bell_test_circuit():
+    alice = cirq.GridQubit(0, 0)
+    bob = cirq.GridQubit(1, 0)
+    alice_referee = cirq.GridQubit(0, 1)
+    bob_referee = cirq.GridQubit(1, 1)
+    circuit = cirq.Circuit()
+    # Prepare shared entangled state.
+    circuit.append([
+        cirq.H(alice),
+        cirq.CNOT(alice, bob),
+        cirq.X(alice)**-0.25,
+    ])
+    # Referees flip coins.
+    circuit.append([
+        cirq.H(alice_referee),
+        cirq.H(bob_referee),
+    ])
+    # Players do a sqrt(X) based on their referee's coin.
+    circuit.append([
+        cirq.CNOT(alice_referee, alice)**0.5,
+        cirq.CNOT(bob_referee, bob)**0.5,
+    ])
+    # Then results are recorded.
+    circuit.append([
+        cirq.measure(alice, key='a'),
+        cirq.measure(bob, key='b'),
+        cirq.measure(alice_referee, key='x'),
+        cirq.measure(bob_referee, key='y'),
+    ])
+    return circuit
+
+def bitstring(bits):
+    return ''.join('1' if e else '_' for e in bits)
 
 def main():
     # Create circuit.
@@ -32,7 +64,7 @@ def main():
 
     # Run simulations.
     print()
-    repetitions = 75
+    repetitions = 100
     print('Simulating {} repetitions...'.format(repetitions))
     result = cirq.Simulator().run(program=circuit, repetitions=repetitions)
 
@@ -51,51 +83,9 @@ def main():
     print('b:', bitstring(b))
     print('x:', bitstring(x))
     print('y:', bitstring(y))
-    print('(a XOR b) == (x AND y):\n  ', bitstring(outcomes))
+    print('(a XOR b) == (x AND y):') 
+    print('B:',bitstring(outcomes))
     print('Win rate: {}%'.format(win_percent))
-
-
-def make_bell_test_circuit():
-    alice = cirq.GridQubit(0, 0)
-    bob = cirq.GridQubit(1, 0)
-    alice_referee = cirq.GridQubit(0, 1)
-    bob_referee = cirq.GridQubit(1, 1)
-
-    circuit = cirq.Circuit()
-
-    # Prepare shared entangled state.
-    circuit.append([
-        cirq.H(alice),
-        cirq.CNOT(alice, bob),
-        cirq.X(alice)**-0.25,
-    ])
-
-    # Referees flip coins.
-    circuit.append([
-        cirq.H(alice_referee),
-        cirq.H(bob_referee),
-    ])
-
-    # Players do a sqrt(X) based on their referee's coin.
-    circuit.append([
-        cirq.CNOT(alice_referee, alice)**0.5,
-        cirq.CNOT(bob_referee, bob)**0.5,
-    ])
-
-    # Then results are recorded.
-    circuit.append([
-        cirq.measure(alice, key='a'),
-        cirq.measure(bob, key='b'),
-        cirq.measure(alice_referee, key='x'),
-        cirq.measure(bob_referee, key='y'),
-    ])
-
-    return circuit
-
-
-def bitstring(bits):
-    return ''.join('1' if e else '_' for e in bits)
-
 
 if __name__ == '__main__':
     main()
